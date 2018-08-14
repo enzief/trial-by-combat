cc23f87f47a7c3d2ece6b08600a3c23d75a9c4a7
Yui Pham <duyp@cakesolutions.net>
2018-08-14 18:57:40 +0100

    Bellman-Ford (#2)
    * SwissBog test
    
    * Refactor
    
    * Complexity
    
    * Real data
    
    * IO up
    
    * Ammonite
    
    * Funky
    
    * Using Bellman-Ford
    
    * Funkify ammonite
    
    * credit
    
    * Remove redundant check

--- src/main/scala/swissbog/BellmanFord.scala
+++ src/main/scala/swissbog/BellmanFord.scala
@@ -1,33 +1,6 @@
-import $ivy.`org.typelevel::cats-core:1.2.0`
-import $ivy.`org.typelevel::cats-effect:0.10.1`
-import $ivy.`io.circe::circe-generic:0.9.3`
-import $ivy.`io.circe::circe-literal:0.9.3`
-import $ivy.`io.circe::circe-parser:0.9.3`
-import $ivy.`org.http4s::http4s-blaze-client:0.18.15`
-import $ivy.`org.http4s::http4s-circe:0.18.15`
-import $ivy.`org.http4s::http4s-dsl:0.18.15`
+// Copyright (c) 2018 Round Inc.. All rights reserved.
 
-import cats.effect.IO
-import cats.implicits._
-import io.circe.KeyDecoder
-import org.http4s.EntityDecoder
-import org.http4s.circe._
-import org.http4s.client.blaze._
-
-final case class Currency(name: String) extends AnyVal {
-  override def toString: String = name
-}
-
-final case class Trade(from: Currency, to: Currency, rate: Double)
-
-final case class Arbitrage(exchanges: Vector[Trade]) extends AnyVal {
-
-  def profit: Double = exchanges.map(_.rate).reduce(_ * _)
-
-  override def toString: String = exchanges.mkString("", " -> ", s" -> $profit")
-}
-
-type Rates = Map[(Currency, Currency), Double]
+package swissbog
 
 object Graph {
   type Vertex   = Currency
@@ -44,10 +17,9 @@ object Graph {
   def findLoop(edges: Edges): Option[Vector[Vertex]] =
     for {
       ((src, _), _) <- edges.headOption
-      g             =  buildGraph(src, edges)
-      ls            <- algorithmA(g).orElse(algorithmB(g))
+      g = buildGraph(src, edges)
+      ls <- algorithmA(g).orElse(algorithmB(g))
     } yield ls
-
 
   /** Finding the first outlier edge needs O(E), plus collecting cyclic
     * vertice complexity O(V^2/2). All are superseded by O(EV^2) of the
@@ -121,60 +93,3 @@ object Graph {
       }
     }
 }
-
-object Algebra {
-
-  /**
-    * An implementation of:
-    * https://math.stackexchange.com/questions/94414/an-algorithm-for-arbitrage-in-currency-exchange
-    *  and
-    * http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.86.1981&rep=rep1&type=pdf
-    *
-    * Finding a trading loop has time complexity of O(EV^2) where V is the number of currencies
-    * and E is the number of rate entries.
-    * Then for the loop, O(EV) is needed to transform it into an `Arbitrage`. Therefore, the total
-    * complexity of arbitrage finding is O(EV^2).
-    */
-  def findArbitrages(rates: Rates): Option[Arbitrage] =
-    Graph.findLoop(rates.mapValues(-math.log(_))).map { loop =>
-      val pairs = loop
-        .sliding(2)
-        .map(s => s.head -> s.tail.head)
-      val trades = pairs.foldLeft(Vector.empty[Trade]) { (trades, pair) =>
-        trades ++ rates
-          .get(pair)
-          .map(Trade(pair._1, pair._2, _))
-      }
-      Arbitrage(trades)
-    }
-
-  implicit val currencyDecoder: KeyDecoder[(Currency, Currency)] = new KeyDecoder[(Currency, Currency)] {
-    override def apply(key: String): Option[(Currency, Currency)] = {
-      val Array(from, to) = key.split("_")
-      Some(Currency(from) -> Currency(to))
-    }
-  }
-
-  implicit val ratesDecoder: EntityDecoder[IO, Rates] = jsonOf
-
-  def getRates: IO[Rates] =
-    for {
-      client <- Http1Client[IO]()
-      rates  <- client.expect[Rates]("https://fx.priceonomics.com/v1/rates/")
-    } yield {
-      client.shutdownNow()
-      rates.filterKeys(k => k._1 != k._2)
-    }
-}
-
-import Algebra._
-
-getRates
-  .attempt
-  .unsafeRunSync()
-  .leftMap(e => "Failed with error: " + e.getMessage)
-  .flatMap {
-    findArbitrages(_).toRight("No arbitrage")
-  }
-  .leftMap(println)
-  .foreach(println)
